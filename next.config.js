3e23e24c62ac1193d9b59118740d21871e2d0241
adairmx <adairmx2013@gmail.com>
2026-02-03 00:40:19 +0000

    Switch to static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
+  distDir: 'out',
   images: {
+    unoptimized: true,
     remotePatterns: [],
     formats: ['image/webp', 'image/avif'],
   },
