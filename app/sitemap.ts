900aa858af6332e10297143609772d101507f116
adairmx <adairmx2013@gmail.com>
2026-02-03 00:41:28 +0000

    Add force-static export for robots.txt and sitemap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { bajioStates } from '@/lib/states'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://stps-agentes-capacitadores.com'
   
